a3c45b94dc50e0d698685a35990f21df0c135250
yuhui Hua <2826537460@qq.com>
2021-09-25 12:50:52 +0800

    Refactor password check program to use functions

--- prac03/password_check.py
+++ prac03/password_check.py
@@ -1,5 +1,13 @@
-password = input("What's your password?")
-while (len(password) < 6):
-    password = input("the length can not be less than 6!your password is")
-for i in range(0, len(password), 1):
-    print("*", end="")
+def main():
+    password=get_password()
+    print_asterisks(password)
+def get_password():
+     getpassword=input("What's your password?")
+     while (len(getpassword) < 6):
+         getpassword = input("the length can not be less than 6!your password is")
+     return getpassword
+def print_asterisks(password):
+    for i in range(0, len(password), 1):
+        print("*", end="")
+main()
+
